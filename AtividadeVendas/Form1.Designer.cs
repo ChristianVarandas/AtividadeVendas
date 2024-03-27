8b6ca729803113a22d7e0cb3a09734bce70abbaf
ChristianVarandas <christianvarandas@etec.sp.gov.br>
2024-03-27 10:39:05 -0300

    AtividadeVendas

---
+++ AtividadeVendas/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace AtividadeVendas
+{
+    partial class Frm1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(11, 13);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(64, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Salário Fixo:";
+            this.lbl1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(11, 78);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(158, 13);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Quantidade de carros vendidos:";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(11, 45);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(79, 13);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "Valor por carro:";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(11, 110);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(92, 13);
+            this.lbl4.TabIndex = 3;
+            this.lbl4.Text = "Valor das vendas:";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(415, 19);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(64, 13);
+            this.lbl5.TabIndex = 4;
+            this.lbl5.Text = "Salário final:";
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Location = new System.Drawing.Point(561, 19);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(37, 13);
+            this.lbl6.TabIndex = 5;
+            this.lbl6.Text = "Bônus";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(81, 7);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(91, 20);
+            this.txt1.TabIndex = 6;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(96, 42);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(91, 20);
+            this.txt2.TabIndex = 7;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(175, 75);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 20);
+            this.txt3.TabIndex = 8;
+            this.txt3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(109, 107);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(100, 20);
+            this.txt4.TabIndex = 9;
+            // 
+            // txt5
+            // 
+            this.txt5.Location = new System.Drawing.Point(397, 45);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(100, 20);
+            this.txt5.TabIndex = 10;
+            // 
+            // txt6
+            // 
+            this.txt6.Location = new System.Drawing.Point(530, 45);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(100, 20);
+            this.txt6.TabIndex = 11;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(52, 145);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(135, 36);
+            this.btn1.TabIndex = 12;
+            this.btn1.Text = "Calcular";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Frm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Frm1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ AtividadeVendas/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtividadeVendas
+{
+    public partial class Frm1 : Form
+    {
+        public Frm1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+            double sf, vc, qcv, vt, result, result2;
+            sf = Convert.ToDouble(txt1.Text);
+            vc = Convert.ToDouble(txt2.Text);
+            qcv = Convert.ToDouble(txt3.Text);
+            vt = Convert.ToDouble(txt4.Text);
+            result = sf + vc + vt;
+            txt5.Text = result.ToString();
+            result2 = vt * 5 / 100;
+            txt6.Text = result2.ToString();
+
+        }
+    }
+    }
